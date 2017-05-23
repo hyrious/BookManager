7f01f64cb87e30912eed13c2475091b1d2bae653
hyrious <hyrious@outlook.com>
2017-05-23 22:46:31 +0800

    added test tinydsl

--- batake.cmd
+++ batake.cmd
@@ -2,13 +2,15 @@
 setlocal EnableDelayedExpansion
 if "%1" equ "" call :default 2>nul && goto :EOF
 :mainloop { handle args.shift }
-if /i "%1" neq "" call :%~1 && shift && goto mainloop else call :default 2>nul
+if /i "%1" neq "" call :%~1 && shift && goto mainloop
                     rem ^ it's unsafe :/
 goto :EOF
 
 :default
 :test hello world, lol
+setlocal DisableDelayedExpansion
 echo Hello world!
+setlocal EnableDelayedExpansion
 goto :EOF
 
 :clean
