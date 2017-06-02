ea357661e6a0567a101820d69aae0852d03596e9
hyrious <hyrious@outlook.com>
2017-06-03 01:07:10 +0800

    pretend to use reflection XD

--- src/bkmgr/Main.java
+++ src/bkmgr/Main.java
@@ -1,20 +1,31 @@
 package bkmgr;
 
-import bkmgr.scene.SceneBorrow;
-import bkmgr.scene.SceneChangePassword;
-import bkmgr.scene.SceneLogin;
-import bkmgr.scene.SceneManageBook;
-import bkmgr.scene.SceneNavigator;
+import java.lang.reflect.InvocationTargetException;
+import java.net.URL;
+import bkmgr.scene.SceneBase;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    private static String camelize(final String line) {
+        return Character.toLowerCase(line.charAt(0)) + line.substring(1);
+    }
+    private void register(final String id) {
+        try {
+            SceneManager.set(camelize(id), (SceneBase) Class.forName("bkmgr.scene.Scene" + id).getConstructor(URL.class)
+                    .newInstance(getClass().getResource("UI" + id + ".fxml")));
+        } catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
+                 | NoSuchMethodException | SecurityException | ClassNotFoundException e) {
+            System.err.println("What happened when register \"" + id + "\" ?");
+            e.printStackTrace();
+        }
+    }
+    private void register(final String... ids) {
+        for (String id : ids)
+            register(id);
+    }
     @Override public void start(Stage window) {
-        SceneManager.set("login", new SceneLogin(getClass().getResource("UILogin.fxml")));
-        SceneManager.set("navigator", new SceneNavigator(getClass().getResource("UINavigator.fxml")));
-        SceneManager.set("borrow", new SceneBorrow(getClass().getResource("UIBorrow.fxml")));
-        SceneManager.set("changePassword", new SceneChangePassword(getClass().getResource("UIChangePassword.fxml")));
-        SceneManager.set("manageBook", new SceneManageBook(getClass().getResource("UIManageBook.fxml")));
+        register("Login", "Navigator", "Borrow", "ChangePassword", "ManageBook");
         SceneManager.start(window, "login");
     }
     public static void main(String[] args) {
